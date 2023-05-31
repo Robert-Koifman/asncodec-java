47a6cab84cc7ba7a574296b8a3b485d4f6fdbe75
Robert Koifman <robert.koifman@gmail.com>
2023-05-31 15:13:55 +0500

    A license notice is included into each file to indicate that the file is subject to the GNU GPL v3.

--- src/softnet/asn/EndOfContainerAsnException.java
+++ src/softnet/asn/EndOfContainerAsnException.java
@@ -1,3 +1,22 @@
+/*
+*	Copyright (c) 2023 Robert Koifman 
+*	
+*	This file is part of Softnet ASN.1 Codec (Java).
+*	
+*	Softnet ASN.1 Codec (Java) is free software: you can redistribute it and/or modify
+*	it under the terms of the GNU General Public License as published by
+*	the Free Software Foundation, either version 3 of the License, or
+*	(at your option) any later version.
+*	
+*	Softnet ASN.1 Codec (Java) is distributed in the hope that it will be useful,
+*	but WITHOUT ANY WARRANTY; without even the implied warranty of
+*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+*	GNU General Public License for more details.
+*	
+*	You should have received a copy of the GNU General Public License
+*	along with Softnet ASN.1 Codec (Java). If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package softnet.asn;
 
 public class EndOfContainerAsnException extends AsnException
